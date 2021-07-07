1183c5a5f22c417f8034c1cd1852580f2ee17cef
Mickey <mklpan13@gmail.com>
2021-07-07 16:54:14 -0400

    Favorites

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -10,20 +10,19 @@ class Contact extends Component {
 
     render() {
         return (
-            <ScrollView wrapperStyle={{ margin: 20 }}>
+            <ScrollView style={{backgroundColor: 'gray'}}>
                 <Card
+                    wrapperStyle={{ margin: 20 }}
                     title="Contact Information">
-                    <Card>
-                        <Text style={{ marginBottom: 10 }}>
-                            1 Nucamp Way{"\n"}
-                            Seattle, WA 98001{"\n"}
-                            U.S.A.
-                        </Text>
-                        <Text>
-                            Phone: 1-206-555-1234{"\n"}
-                            Email: campsites@nucamp.co
-                        </Text>
-                    </Card>
+                    <Text style={{ marginBottom: 10 }}>
+                        1 Nucamp Way{"\n"}
+                        Seattle, WA 98001{"\n"}
+                        U.S.A.
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        Phone: 1-206-555-1234{"\n"}
+                        Email: campsites@nucamp.co
+                    </Text>
                 </Card>
             </ScrollView>
         );
